e8d2538e57b7cbcecfa8e9204ca68f34306ef190
Jesse Watts-Russell <jwattsrussel@feverfew.cs.hmc.edu>
2014-04-24 16:24:06 -0700

    moo

---
+++ src/yahtzee_manager.erl
@@ -0,0 +1,23 @@
+%%% ---------------------------------------------------------------------------
+%%% CSCI182E - Distributed Systems
+%%% @author Jesse Watts-Russell, David Scott, and Alex Melville
+%%% @copyright ChickenFartStory Inc. 2015, All rights reserved
+%%%
+%%% @doc A Generic Server implementation of a Tournament Manager according
+%%%		 to OTP principles.
+%%% @end
+%%%----------------------------------------------------------------------------
+
+-module(controller).
+-behavior(gen_server).
+
+%% External exports
+-export([]).
+
+%% gen_server callbacks
+-export([init/1, 
+		 handle_call/3, 
+		 handle_cast/2, 
+		 handle_info/2, 
+		 code_change/3, 
+		 terminate/2]).
