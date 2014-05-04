c8e29f0c989c4a944751b77ce08cb5b8cf27daea
David Scott <dscott@hmc.edu>
2014-05-03 23:52:55 -0700

    FUCK

--- src/yahtzee_manager.erl
+++ src/yahtzee_manager.erl
@@ -1093,9 +1093,9 @@ match_ended( Tid, #match{p1Win = P1Win, p2Win = P2Win, p1 = P1, p2 = P2} )
     	    P2Pid ! {end_tournament, self(), P2, Tid},
 
     	    %% The tournament is over, Player 1 won, update the tournament
-    	    ets:insert(Tid, T#tournament{bracket = NewBracket,
-    					 status = completed,
-    					 winner = P2});
+    	    ets:insert(?TournamentInfo, {Tid, T#tournament{bracket = NewBracket,
+							   status = completed,
+							   winner = P2}});
     	none ->
     	    %% The next round opponent hasn't finished the previous match yet
     	    %% Do nothing...
